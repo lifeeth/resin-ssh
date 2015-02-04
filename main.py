9fec56240fd35c4320915405888a0c59068fa375
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-02-04 17:29:48 +0000

    added a little flask server

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-while True:
-	print "hello python!"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
