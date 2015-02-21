da5db04a63e13c9a1e3038939c0991ddabe21ac6
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-02-21 15:38:08 +0000

    bump raspbian version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM resin/rpi-raspbian:wheezy-2015-01-21
+FROM resin/rpi-raspbian:latest
 
 RUN apt-get update && apt-get install -y python python-dev python-pip
 
--- start.sh
+++ start.sh
@@ -6,12 +6,5 @@ echo "root:$PASSWD" | chpasswd
 #Spawn dropbear
 dropbear -E -F &
 
-#enable i2c access module in kernel if needed.
-#modprobe i2c-bcm2708
-#modprobe i2c-dev
-
-#enable watchdog module in kernel if needed.
-#modprobe bcm2708_wdog
-
 #start your application from here...
 python app/main.py
